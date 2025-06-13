ed94466e1553ce82424a5421ba38884fda44e1c1
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2025-06-13 20:12:00 +0530

    fix

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -10,7 +10,7 @@ const ProjectCard = ({ project }) => {
           className="rounded-lg w-full h-48 object-cover mb-4"
         />
       )}
-      <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
+      <h3 className="text-gray-600 text-xl font-semibold mb-2">{project.title}</h3>
       <p className="text-gray-600 mb-3">{project.description}</p>
       <div className="flex flex-wrap gap-2 mb-4">
         {project.technologies.map((tech, idx) => (
